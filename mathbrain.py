652fddbc608a18df265b03c2901b002bd19bc32d
Herbert Yeung <debouncing@gmail.com>
2012-04-25 14:50:25 +1000

    Fixing qInequalities template to be in mathml format

--- mathbrain.py
+++ mathbrain.py
@@ -6,11 +6,12 @@ import asciimathml as am
 from xml.etree.ElementTree import tostring
 from jinja2 import Environment, PackageLoader
 from sympy import solve, Poly, Eq, Function, exp, Le, Lt, Ge, Gt
+from sympy.printing import mathml
 from sympy.abc import x
 from random import choice, randint
 from math import pow
 
-def qInequalities_template():
+def qSignificantFigures_template():
     '''Solve inequalities. e.g. 2-3x <= 8.'''
     leftside_section1 = randint(-100,100)
     leftside_section2 = randint(-100,100)
@@ -39,6 +40,40 @@ def qInequalities_template():
     answer.append(solve(question, x))
 
     return question, answer
+
+
+def qInequalities_template():
+    '''Solve inequalities. e.g. 2-3x <= 8.'''
+    leftside_section1 = randint(-100,100)
+    leftside_section2 = randint(-100,100)
+    left_side = leftside_section1 + leftside_section2
+    right_side = randint(-100,100)
+    equality_type = randint(0,3) #<, <=, >, >=
+    question = None 
+    question_str = "Solve : "
+    if equality_type == 0:
+        question = Lt(leftside_section1 + leftside_section2*x, right_side)
+    elif equality_type == 1:
+        question = Le(leftside_section1 + leftside_section2*x, right_side)
+    elif equality_type == 2:
+        question = Gt(leftside_section1 + leftside_section2*x, right_side)
+    elif equality_type == 3:
+        question = Ge(leftside_section1 + leftside_section2*x, right_side)
+    question_str += mathml(question)
+
+    steps = []
+    if leftside_section1 < 0:
+        steps.append('Move by +' + str(leftside_section1*-1) + ' to both ' \
+                     +'sides')
+    else:
+        steps.append('Move by -' + str(leftside_section1) + ' to both ' \
+                     +'sides')
+    steps.append('Divide left and right side by ' + str(leftside_section2))
+    answer = []
+    answer.append(steps)
+    answer.append(mathml(solve(question, x)))
+
+    return question_str, answer
 
 def qExponentialSameBase_template():
     '''Solves the same base e.g. 2^(2x+1) = 32.'''
