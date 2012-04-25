90f31f84aa87041050f44dc608ab7043b9201674
Herbert Yeung <debouncing@gmail.com>
2012-04-25 14:31:10 +1000

    Adding qInequalities template

--- mathbrain.py
+++ mathbrain.py
@@ -5,16 +5,40 @@ import asciimathml as am
 
 from xml.etree.ElementTree import tostring
 from jinja2 import Environment, PackageLoader
+from sympy import solve, Poly, Eq, Function, exp, Le, Lt, Ge, Gt
+from sympy.abc import x
+from random import choice, randint
+from math import pow
 
 def qInequalities_template():
-    '''Solve inequalities.'''
-    env = Environment(loader=PackageLoader('mathbrain', 'templates'))
-    template = env.get_template('template1.html')
-    from random import choice, randint
-    from math import pow
-    from sympy import solve, Poly, Eq, Function, exp
-    from sympy.abc import x
-    
+    '''Solve inequalities. e.g. 2-3x <= 8.'''
+    leftside_section1 = randint(-100,100)
+    leftside_section2 = randint(-100,100)
+    left_side = leftside_section1 + leftside_section2
+    right_side = randint(-100,100)
+    equality_type = randint(0,3) #<, <=, >, >=
+    question = None 
+    if equality_type == 0:
+        question = Lt(leftside_section1 + leftside_section2*x, right_side)
+    elif equality_type == 1:
+        question = Le(leftside_section1 + leftside_section2*x, right_side)
+    elif equality_type == 2:
+        question = Gt(leftside_section1 + leftside_section2*x, right_side)
+    elif equality_type == 3:
+        question = Ge(leftside_section1 + leftside_section2*x, right_side)
+    steps = []
+    if leftside_section1 < 0:
+        steps.append('Move by +' + str(leftside_section1*-1) + ' to both ' \
+                     +'sides')
+    else:
+        steps.append('Move by -' + str(leftside_section1) + ' to both ' \
+                     +'sides')
+    steps.append('Divide left and right side by ' + str(leftside_section2))
+    answer = []
+    answer.append(steps)
+    answer.append(solve(question, x))
+
+    return question, answer
 
 def qExponentialSameBase_template():
     '''Solves the same base e.g. 2^(2x+1) = 32.'''
@@ -40,7 +64,10 @@ def main():
     '''Generating a template'''
     question = []
     answer = []
-    q, a = qExponentialSameBase_template()
+    #q, a = qExponentialSameBase_template()
+    #question.append(q)
+    #answer.append(a)
+    q, a = qInequalities_template()
     question.append(q)
     answer.append(a)
     print question
