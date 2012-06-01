cd991ced97a8a3c81bd611184bd59a7d7367b6df
Shaon Diwakar <shaon@shaon.net>
2012-06-02 01:51:02 +1000

    Archiving the old pinax site.

--- site/mathbrain_old/apps/timer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-
-
-urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {"template": "timer/2u.html"}, name="2utimer"),
-    url(r"^2u/$", direct_to_template, {"template": "timer/2u.html"}, name="2utimer"),
-    url(r"^3u/$", direct_to_template, {"template": "timer/3u.html"}, name="3utimer"),
-)
