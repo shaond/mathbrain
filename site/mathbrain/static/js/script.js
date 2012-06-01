64659663de7d75dc98570670f9aff67bd71ea6ba
Shaon Diwakar <shaon@shaon.net>
2012-06-02 03:44:46 +1000

    The page now shows how many marks the question is worth.
    	* timer.html - marks now pulled from json object
    	* script.js - adding some skeleton code

--- site/mathbrain/static/js/script.js
+++ site/mathbrain/static/js/script.js
@@ -1,9 +1,20 @@
-/* Author:
 
-*/
+// Set some common variables
+var TOTALMARKS = 12;
+var TOTALQUESTIONS = 12;
 
 
+var currentQuestion = function(question) {
+    // Gets what questions we have done
+    if (typeof question === 'number') {
+        this.question = question;
+    }
+};
 
+
+var currentMarks = function() {
+    // Gets the number of marks we've run so far 
+};
 
 
 
