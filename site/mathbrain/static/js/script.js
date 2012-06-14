5c6be4c35c1b8968381466eb2b97873943af5899
Shaon Diwakar <shaon@shaon.net>
2012-06-15 01:07:40 +1000

    Starting to work on local storage & getting images to preload.
    Even though nothing is working, the site should still be fully functional.

--- site/mathbrain/static/js/script.js
+++ site/mathbrain/static/js/script.js
@@ -1,1 +1,16 @@
+/* Most of our external functions */
 
+var saveQuestions = function(questions) {
+    // saves the questions JSON object using local storage
+    localStorage.setItem("questions", questions);
+};
+
+var prefetchImages = function(questions) {
+    // prefetch all images for this exam
+    URL = '/questions/';
+    if (questions.length > 0){
+        for (var i; i < questions.length; i++) {
+            $.ajax({url: URL + questions[i].fields.img, cache: true, async: true, success: function() {} });        
+        }
+    }
+};
