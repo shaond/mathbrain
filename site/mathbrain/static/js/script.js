5db4a4918ed3bea3b3d79b5dc821a5f6bbdb92c2
Herbert Yeung <debouncing@gmail.com>
2012-06-17 02:01:55 +1000

    Adding ability to store time to localStorage. Also, we now retrieve this value back in displayQuestion().

--- site/mathbrain/static/js/script.js
+++ site/mathbrain/static/js/script.js
@@ -2,15 +2,41 @@
 
 var saveQuestions = function(questions) {
     // saves the questions JSON object using local storage
-    localStorage.setItem("questions", questions);
+    if (localStorage) {
+        localStorage.setItem("questions", questions);
+    }
 };
 
 var prefetchImages = function(questions) {
     // prefetch all images for this exam
     URL = '/questions/';
-    if (questions.length > 0){
+    if (questions.length > 0) {
         for (var i; i < questions.length; i++) {
-            $.ajax({url: URL + questions[i].fields.img, cache: true, async: true, success: function() {} });        
+            $.ajax({
+		url: URL + questions[i].fields.img, 
+		cache: true, 
+		async: true, 
+	        success: function() {
+		   //Insert into the DOM but display as NONE 
+		   //This 'caches' image in DOM so when it's called it is available 
+		   //$(this).
+
+	    }});        
         }
+    }
+};
+
+var recordQuestionTime = function(qid, qtime) {
+    // Save the time
+    if (localStorage) {
+        localStorage.setItem(qid, qtime);
+    }
+};
+
+// This should only be called in onbeforeunload
+var recordTotalTime = function(ttime) {
+    // Save the time
+    if (localStorage) {
+        localStorage.setItem("total_time", ttime);
     }
 };
